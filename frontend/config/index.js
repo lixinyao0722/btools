001e60ee69bca01e3f5b3230fffa33b0d1e80dca
xinyaoli(李新耀) <xinyaoli@tencent.com>
2018-04-11 20:07:35 +0800

    up

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/weblab',
     proxyTable: {},
 
     // Various Dev Server settings
