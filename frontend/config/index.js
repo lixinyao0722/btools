ddd61365cc98bedb4800a3017d9d4d13b91ae167
xinyaoli(李新耀) <xinyaoli@tencent.com>
2018-04-11 20:14:46 +0800

    up

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/weblab',
+    assetsPublicPath: '/weblab/',
 
     /**
      * Source Maps
